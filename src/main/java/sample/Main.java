43092a3c977e10c1c9369a5f78cda622eef49a4b
Maciej Romaniuk <s22107@pjwstk.edu.pl>
2021-09-07 21:29:33 +0200

    Checking for more complex views. Looks good.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,7 +1,7 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -12,9 +12,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
         HashMap<String, Object> x = new HashMap<>();
         x.put("first", "data");
